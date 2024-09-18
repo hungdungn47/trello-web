2f94a2ff8bda64e550c6cc1f48ed82ec63f2d508
hungdungn47 <hungdungn47@gmail.com>
2024-09-18 07:51:15 +0700

    Dark light mode

--- src/theme.js
+++ src/theme.js
@@ -1,17 +1,15 @@
-import { createTheme } from '@mui/material/styles'
-import { red } from '@mui/material/colors'
+import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
 
 // Create a theme instance.
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#556cd6'
+const theme = extendTheme({
+  colorSchemes: {
+    light: {
+      palette: {
+      }
     },
-    secondary: {
-      main: '#19857b'
-    },
-    error: {
-      main: red.A400
+    dark: {
+      palette: {
+      }
     }
   }
 })
