434a7d1699ba4788f4caf2ceecb976cd69ce7910
hungdungn47 <hungdungn47@gmail.com>
2024-10-07 09:31:58 +0700

    test api

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -2,14 +2,23 @@ import { Container } from '@mui/material'
 import AppBar from '~/components/AppBar/AppBar'
 import BoardBar from './BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
-import { mockData } from '../../apis/mock_data'
+import { useEffect, useState } from 'react'
+import { fetchBoardDetailsAPI } from '../../apis'
 
 const Board = () => {
+  const [board, setBoard] = useState(null)
+
+  useEffect(() => {
+    const boardId = '67016b77d2e0e1f8142f8784'
+    fetchBoardDetailsAPI(boardId).then(() => {
+      setBoard(board)
+    })
+  }, [])
   return (
     <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
       <AppBar/>
-      <BoardBar board={mockData?.board}/>
-      <BoardContent board={mockData?.board}/>
+      <BoardBar board={board}/>
+      <BoardContent board={board}/>
     </Container>
   )
 }
