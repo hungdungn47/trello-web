d14b27762aa39723afa7998f557f1bc9c1714d75
hungdungn47 <hungdungn47@gmail.com>
2025-02-12 15:00:39 +0700

    Install and config redux store, create activeBoardSlice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,1 +1,8 @@
-// State management
+import { configureStore } from "@reduxjs/toolkit";
+import { activeBoardReducer } from "./activeBoard/activeBoardSlice";
+
+export const store = configureStore({
+  reducer: {
+    activeBoard: activeBoardReducer
+  },
+});
