ab6a3e4b470998d62b9df7cd807963f647f8f93c
hungdungn47 <hungdungn47@gmail.com>
2025-02-13 15:20:43 +0700

    Create Login and Register page; Form handling with react hook form

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Board from './pages/Boards/_id'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import NotFound from './pages/NotFound/NotFound'
+import Auth from '~/pages/Auth/Auth'
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Navigate to='/boards/670901d62903edebcb188b99' replace={true} />} />
       <Route path='/boards/:boardId' element={<Board />} />
+      <Route path='/login' element={<Auth />} />
+      <Route path='/register' element={<Auth />} />
       <Route path='*' element={<NotFound />} />
     </Routes>
   )
