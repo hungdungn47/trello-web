92f7a45d9f45999a1240bbd57c5abd64c7a50692
hungdungn47 <hungdungn47@gmail.com>
2025-02-13 14:17:01 +0700

    Not found page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Board from './pages/Boards/_id'
 import { Routes, Route, Navigate } from 'react-router-dom'
+import NotFound from './pages/NotFound/NotFound'
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Navigate to='/boards/670901d62903edebcb188b99' replace={true} />} />
       <Route path='/boards/:boardId' element={<Board />} />
-      <Route path='*' element={<div>Not found</div>} />
+      <Route path='*' element={<NotFound />} />
     </Routes>
   )
 }
