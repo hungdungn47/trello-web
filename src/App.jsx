e250d7453bc7f0581efb2670ef978a3965451d11
hungdungn47 <hungdungn47@gmail.com>
2025-02-13 14:01:34 +0700

    Use browser router of React router dom for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Board from './pages/Boards/_id'
+import { Routes, Route, Navigate } from 'react-router-dom'
 
 function App() {
   return (
-    <>
-      <Board/>
-    </>
+    <Routes>
+      <Route path='/' element={<Navigate to='/boards/670901d62903edebcb188b99' replace={true} />} />
+      <Route path='/boards/:boardId' element={<Board />} />
+      <Route path='*' element={<div>Not found</div>} />
+    </Routes>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "~/App.jsx";
-import CssBaseline from "@mui/material/CssBaseline";
-import { Experimental_CssVarsProvider as CssVarsProvider } from "@mui/material/styles";
-import theme from "~/theme";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import { ConfirmProvider } from "material-ui-confirm";
-import { store } from "~/redux/store";
-import { Provider } from "react-redux";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "~/App.jsx"
+import CssBaseline from "@mui/material/CssBaseline"
+import { Experimental_CssVarsProvider as CssVarsProvider } from "@mui/material/styles"
+import theme from "~/theme"
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
+import { ConfirmProvider } from "material-ui-confirm"
+import { store } from "~/redux/store"
+import { Provider } from "react-redux"
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <CssVarsProvider theme={theme}>
-      <ConfirmProvider>
-        <CssBaseline />
-        <App />
-        <ToastContainer position="bottom-right" theme="colored" />
-      </ConfirmProvider>
-    </CssVarsProvider>
-  </Provider>
+  <BrowserRouter basename="/">
+    <Provider store={store}>
+      <CssVarsProvider theme={theme}>
+        <ConfirmProvider>
+          <CssBaseline />
+          <App />
+          <ToastContainer position="bottom-right" theme="colored" />
+        </ConfirmProvider>
+      </CssVarsProvider>
+    </Provider>
+  </BrowserRouter>
 );
